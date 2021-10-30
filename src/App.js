48a0fb1d04f39029c5abc44e3b7166ecd9173038
najmus2808 <najmus2808@gmail.com>
2021-10-30 23:53:30 +0600

    search-product project is done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
 import React from "react";
+import FilterableProductTable from "./components/FilterableProductTable";
 export default class App extends React.Component {
   render() {
+    const PRODUCTS = [
+      {
+        category: "Sporting Goods",
+        price: "$49.99",
+        stocked: true,
+        name: "Football",
+      },
+      {
+        category: "Sporting Goods",
+        price: "$9.99",
+        stocked: true,
+        name: "Baseball",
+      },
+      {
+        category: "Sporting Goods",
+        price: "$29.99",
+        stocked: false,
+        name: "Basketball",
+      },
+      {
+        category: "Electronics",
+        price: "$99.99",
+        stocked: true,
+        name: "iPod Touch",
+      },
+      {
+        category: "Electronics",
+        price: "$399.99",
+        stocked: false,
+        name: "iPhone 5",
+      },
+      {
+        category: "Electronics",
+        price: "$199.99",
+        stocked: true,
+        name: "Nexus 7",
+      },
+    ];
     return (
-      <div className="app" style={{ textAlign: "center" }}>S</div>
+      <div className="app">
+        <FilterableProductTable products={PRODUCTS} />
+      </div>
     );
   }
 }
